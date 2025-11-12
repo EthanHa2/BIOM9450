bbffa2fea2d92cd873cdf61ab4a8cca55722951e
Anish Sanghvi <z5421465@ad.unsw.edu.au>
2025-11-12 21:16:16 +1100

    updating predict to have proper file paths and also tested using a test.php file and it seems to work

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,4 +1,4 @@
 from model import train_random_forest_optimised
 
 # Run initial model
-results = train_random_forest_optimised("Mutation_original.csv")
+results = train_random_forest_optimised("Mutation_original.csv");
--- machine_learning/predict.py
+++ machine_learning/predict.py
@@ -7,9 +7,9 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # Load trained components
-model = joblib.load("model/rf_cancer_predictor.pkl")
-scaler = joblib.load("model/scaler.pkl")
-le_label = joblib.load("model/label_encoder.pkl")
+model = joblib.load("rf_cancer_predictor.pkl")
+scaler = joblib.load("scaler.pkl")
+le_label = joblib.load("label_encoder.pkl")
 
 # Read JSON input from PHP
 input_data = json.loads(sys.stdin.read())
